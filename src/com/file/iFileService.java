739064e4e53930fbe8641a7d078d6dfecaaa3855
anuragbhardwaj <anurag.wizard@gmail.com>
2015-03-01 14:25:25 -0800

    Test

--- src/com/file/iFileService.java
+++ src/com/file/iFileService.java
@@ -5,5 +5,4 @@ public interface iFileService {
 	void shareFile(String userid, String targetUserID, String filepath);
 	void unshareFile(String userId, String targetUserID, String filePath);
 	byte[] readFile(String userId, String filePath);
-	void abc();
 }
